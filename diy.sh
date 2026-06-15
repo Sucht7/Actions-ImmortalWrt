f56d1cd76d35e9962961c49a53cecf80daa07923
Sucht7 <167517865+Sucht7@users.noreply.github.com>
2026-06-15 21:10:49 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 修改默认管理IP 10.0.0.2
+# 修改默认LAN IP 10.0.0.2
 sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
 
 # 设置网关指向主路由 10.0.0.1
@@ -9,6 +9,6 @@ echo "set network.lan.gateway='10.0.0.1'" >> package/base-files/files/bin/config
 # 关闭LAN DHCP（旁路由防冲突）
 sed -i 's/set dhcp.lan.ignore=0/set dhcp.lan.ignore=1/' package/base-files/files/bin/config_generate
 
-# 预设无线国家为 澳大利亚 AU
+# 无线国家码设为 澳大利亚 AU
 mkdir -p etc/config
 echo "wireless country AU" >> etc/config/wireless
