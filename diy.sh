6d28a3e0f15d9cab47ffe3ec2576ff0c8c9d54da
Sucht7 <167517865+Sucht7@users.noreply.github.com>
2026-06-15 20:39:11 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,6 +9,9 @@ echo "set network.lan.gateway='10.0.0.1'" >> package/base-files/files/bin/config
 # 关闭LAN DHCP（旁路由必备）
 sed -i 's/set dhcp.lan.ignore=0/set dhcp.lan.ignore=1/' package/base-files/files/bin/config_generate
 
-# 预设无线国家码为 澳大利亚 (AU) 永久生效
+# 预设无线国家码为 澳大利亚(AU)
 mkdir -p etc/config
 echo "wireless country AU" >> etc/config/wireless
+
+# 【关键修复】删除冲突的 webd 目录，解决 feeds 报错
+rm -rf feeds/small8/webd
