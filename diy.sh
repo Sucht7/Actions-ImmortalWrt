ab440bcbd2c4db495a3b406ea7fb99cc4dc7ce5e
Sucht7 <167517865+Sucht7@users.noreply.github.com>
2026-06-15 18:29:07 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,4 +1,13 @@
-# 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
-# Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
-# git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
-# git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+#!/bin/bash
+# 1. 修改OpenWrt默认IP为 10.0.0.2
+sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
+
+# 2. 设置网关指向iKuai主路由 10.0.0.1
+sed -i '/set network.lan.gateway/d' package/base-files/files/bin/config_generate
+echo "set network.lan.gateway='10.0.0.1'" >> package/base-files/files/bin/config_generate
+
+# 3. 关闭LAN DHCP（旁路由必备，防止和主路由冲突）
+sed -i 's/set dhcp.lan.ignore=0/set dhcp.lan.ignore=1/' package/base-files/files/bin/config_generate
+
+# 4. WiFi国家码改为CN，保证功率、信道合规可用
+sed -i 's/country=US/country/g' package/kernel/mac80211/files/lib/wireless/mac80211.sh
